5a28fee2b713e47c80ea50eaa2c0999b42075d53
Emily Rogers <emilyrogers@mail.usf.edu>
2019-11-30 18:32:06 -0500

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Emily Rogers on 11/30/19.
+//  Copyright © 2019 Emily Rogers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
